9838cf5c1e24deda1d9dece637942fdcb49db0ea
afifhasim <75711212+afifhasim@users.noreply.github.com>
2023-09-02 00:10:01 +0800

    Update Art Gallery Database System.sql

--- Art Gallery Database System.sql
+++ Art Gallery Database System.sql
@@ -4,24 +4,11 @@
 -- ------------------------------------------------------
 -- Server version	8.1.0
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
 --
 -- Table structure for table `artist`
 --
 
 DROP TABLE IF EXISTS `artist`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `artist` (
   `artist` varchar(100) DEFAULT NULL,
   `country` varchar(50) DEFAULT NULL,
@@ -29,15 +16,12 @@ CREATE TABLE `artist` (
   KEY `artist` (`artist`),
   CONSTRAINT `artist_ibfk_1` FOREIGN KEY (`artist`) REFERENCES `arts` (`artist`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `artist`
 --
 
 LOCK TABLES `artist` WRITE;
-/*!40000 ALTER TABLE `artist` DISABLE KEYS */;
-/*!40000 ALTER TABLE `artist` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -45,8 +29,6 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `arts`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `arts` (
   `artist` varchar(100) NOT NULL,
   `code_arts` int DEFAULT NULL,
@@ -56,15 +38,12 @@ CREATE TABLE `arts` (
   KEY `venue` (`venue`),
   CONSTRAINT `arts_ibfk_1` FOREIGN KEY (`venue`) REFERENCES `gallery` (`venue`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `arts`
 --
 
 LOCK TABLES `arts` WRITE;
-/*!40000 ALTER TABLE `arts` DISABLE KEYS */;
-/*!40000 ALTER TABLE `arts` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -72,23 +51,18 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `enquiries`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `enquiries` (
   `register_id` int DEFAULT NULL,
   `first_name` varchar(500) DEFAULT NULL,
   KEY `register_id` (`register_id`),
   CONSTRAINT `enquiries_ibfk_1` FOREIGN KEY (`register_id`) REFERENCES `visitor` (`register_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `enquiries`
 --
 
 LOCK TABLES `enquiries` WRITE;
-/*!40000 ALTER TABLE `enquiries` DISABLE KEYS */;
-/*!40000 ALTER TABLE `enquiries` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -96,8 +70,6 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `gallery`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `gallery` (
   `venue` varchar(15) NOT NULL,
   `arts` varchar(100) DEFAULT NULL,
@@ -106,15 +78,12 @@ CREATE TABLE `gallery` (
   `visitor` int DEFAULT NULL,
   PRIMARY KEY (`venue`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `gallery`
 --
 
 LOCK TABLES `gallery` WRITE;
-/*!40000 ALTER TABLE `gallery` DISABLE KEYS */;
-/*!40000 ALTER TABLE `gallery` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -122,32 +91,18 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `visitor`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `visitor` (
   `register_id` int NOT NULL,
   `first_name` varchar(50) DEFAULT NULL,
   `last_name` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`register_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `visitor`
 --
 
 LOCK TABLES `visitor` WRITE;
-/*!40000 ALTER TABLE `visitor` DISABLE KEYS */;
-/*!40000 ALTER TABLE `visitor` ENABLE KEYS */;
 UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
 -- Dump completed on 2023-09-02  0:05:02
